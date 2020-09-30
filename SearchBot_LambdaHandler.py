dc1cc9f21bd3a2255cef18043af606e5713c5b2a
anuragbisht12 <anuragbisht12@gmail.com>
2020-09-30 05:33:43 +0530

    Create SearchBot_LambdaHandler.py

---
+++ SearchBot_LambdaHandler.py
@@ -0,0 +1,160 @@
+"""
+This sample demonstrates an implementation of the Lex Code Hook Interface
+in order to serve a sample bot which manages orders for flowers.
+Bot, Intent, and Slot models which are compatible with this sample can be found in the Lex Console
+as part of the 'OrderFlowers' template.
+
+For instructions on how to set up and test this bot, as well as additional samples,
+visit the Lex Getting Started documentation http://docs.aws.amazon.com/lex/latest/dg/getting-started.html.
+"""
+import math
+import dateutil.parser
+import datetime
+import time
+import os
+import logging
+import json
+# import urllib.request
+from botocore.vendored import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+api_key = 'PLACEHOLDER'
+service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+
+
+def get_slots(intent_request):
+    return intent_request['currentIntent']['slots']
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+def delegate(session_attributes, slots):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Delegate',
+            'slots': slots
+        }
+    }
+
+
+""" --- Helper Functions --- """
+
+
+def parse_int(n):
+    try:
+        return int(n)
+    except ValueError:
+        return float('nan')
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    if message_content is None:
+        return {
+            "isValid": is_valid,
+            "violatedSlot": violated_slot,
+        }
+
+    return {
+        'isValid': is_valid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 'content': message_content}
+    }
+
+
+def isvalid_date(date):
+    try:
+        dateutil.parser.parse(date)
+        return True
+    except ValueError:
+        return False
+
+""" --- Intents --- """
+
+def search_query(intent_request):
+    
+    query = get_slots(intent_request)["Country"]
+    params = {
+    'query': query,
+    'limit': 10,
+    'indent': True,
+    'key': api_key,
+    }
+    
+    # url = service_url + '?' + urllib.parse.urlencode(params)
+    # response = json.loads(urllib.request.urlopen(url).read())
+    
+    # response=json.loads(requests.get(url))
+    url = service_url
+    response=json.loads(requests.get(url,params=params).content)
+    try:
+        information=response['itemListElement'][0]['result']['detailedDescription']['articleBody']
+
+    except:
+        information='Sorry could not find what you are looking for.'
+    
+    return close(intent_request['sessionAttributes'],
+                 'Fulfilled',
+                 {'contentType': 'PlainText',
+                  'content': information})
+    
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'SearchQuery':
+        return search_query(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
